3569128e71f6b84732b9f4244c0ee6627fea4ad5
Uli Kusterer <ulivc@zathras.de>
2016-05-14 00:33:56 +0200

    Rename from old UKxxx prefix to ULIxxx prefix to make Apple happy. Also added projects for a test app and the actual helper app.

---
+++ ULICrashReporter Helper/ULICrashReporter Helper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ULICrashReporter Helper
+//
+//  Created by Uli Kusterer on 14/05/16.
+//  Copyright © 2016 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ULICrashReporter Helper/ULICrashReporter Helper/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  ULICrashReporter Helper
+//
+//  Created by Uli Kusterer on 14/05/16.
+//  Copyright © 2016 Uli Kusterer. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+@end
